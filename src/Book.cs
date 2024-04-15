5b08a89df3b638e15e6abde8ab278d11e4aca63d
Turki Dhawi <T.Dhawi@outlook.com>
2024-04-15 13:49:09 +0300

    Added Properties for Book class

--- src/Book.cs
+++ src/Book.cs
@@ -1,4 +1,21 @@
-    public class Book
+public class Book
+{
+    public string Id
     {
-
+        get;
+        private set;
     }
+    public string Title
+    {
+        get;
+        private set;
+    }
+    public DateTime Date { get; }
+
+    public Book(string id, string title, DateTime date = default)
+    {
+        Id = id;
+        Title = title;
+        Date = date == default ? DateTime.Now : date;
+    }
+}
