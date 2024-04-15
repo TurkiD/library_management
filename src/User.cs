ddd8d9987a123ee76a6105c93f99709130d6d8c2
Turki Dhawi <T.Dhawi@outlook.com>
2024-04-15 13:50:52 +0300

    Added Properties for User class

--- src/User.cs
+++ src/User.cs
@@ -1,4 +1,21 @@
-public class User 
+public class User
 {
-    
+    public string Id
+    {
+        get;
+        private set;
+    }
+    public string Name
+    {
+        get;
+        private set;
+    }
+    public DateTime Date { get; }
+
+    public User(string id, string name, DateTime date = default)
+    {
+        Id = id;
+        Name = name;
+        Date = date == default ? DateTime.Now : date;
+    }
 }
